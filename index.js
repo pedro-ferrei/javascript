5578c85c3946dfb7e51ccf8fe6472fb469dedba8
pedro augusto <paugusto2702@gamil.com>
2024-04-01 10:26:46 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+<DOCTYPE html>
+<html>
+ <head>
+<meta charset="UTF-8">
+ <titule>Javascript Hello World</titule>
+
+<script>
+ function main () {
+   a=5;
+   b=20;
+   console.log("hello World"+(a+b))
+   alert("Hello World")
+}
+function soma (a,b){
+      retrurn a+b;
+}
+</script>
+</head>
+ <body>
+ 
+</body>
+</html>
